f338cdf1afb4008aea6f201eb84ae90a850e45ac
RynoT <1502306@abertay.ac.uk>
2017-07-14 13:10:50 +0100

    Structures can be supported from above, Build Areas update better#

--- King_of_the_Castle/Source/King_of_the_Castle/Construction/BuildArea.h
+++ King_of_the_Castle/Source/King_of_the_Castle/Construction/BuildArea.h
@@ -53,6 +53,10 @@ public:
 	void OnBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
 		UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+		UPrimitiveComponent *OtherComp, int32 OtherBodyIndex);
+
 #if WITH_EDITOR
 	void PostEditChangeProperty(struct FPropertyChangedEvent& event) override;
 #endif
